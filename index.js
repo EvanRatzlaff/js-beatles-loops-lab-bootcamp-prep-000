bbc76a3404e08594506841572ee765641cf0bc49
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-06-06 20:51:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ function johnLennonFacts(facts){
   do { i++ 
     console.log(last.push("I love the Beatles!"))
   }
-  while (i < num){
-    last.push("I love the Beatles!")
+  while (i < 15){
+    num = 17
   }
   return last
   }
